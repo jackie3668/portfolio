1a27bb848a7de06af9bc5bd95aba2823eb7c4dd3
Jackie <actions@github.com>
2023-12-11 17:50:43 -0500

    Started Hero Section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Hero.css'
+import hero_bg from '../../asset/other/hero_bg.jpg'
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+      <div className="hero-image">
+        <img src={hero_bg} alt="" />
+      </div>
+      <div className="hero-content">
+        <h1>Hi, my name is Jackie Shen</h1>
+      </div>
+    </div>
   )
 }
 
