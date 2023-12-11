76ff2ad35d8e4b2d52cce6760de2ef2590c40bf0
Jackie <actions@github.com>
2023-12-11 17:39:47 -0500

    Navbar Basics Complete

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import './Navbar.css'
+import menu from '../../asset/ui/hamburger.png'
+import close from '../../asset/ui/close.png'
+
+const Navbar = () => {
+  const [menuHidden, setMenuHidden] = useState(true);
+  const [menuClicked, setMenuClicked] = useState(false);
+
+  const handleMobileClick = () => {
+    setMenuClicked(true);
+    setMenuHidden(!menuHidden)
+  };
+
+  return (
+    <div className='navbar'>
+      <div className="navbar-logo"><h2>Jackie Shen</h2></div>
+      <div className="navbar-items-container">
+        <img onClick={handleMobileClick} src={menuHidden ? menu : close} alt="" />
+        <ul className={`navbar-items ${menuClicked && (menuHidden ? 'slide-out-left' : 'slide-in-left')} ${!menuClicked ? 'hidden' : ''}`}>
+          <li>HOME</li>
+          <li>SKILLS</li>
+          <li>MOST RECENT</li>
+          <li>PAST PROJECT</li>
+          <li>CONTACT</li>
+        </ul>
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
