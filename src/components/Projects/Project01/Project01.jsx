cd6192ca6330126c8820b53a3b4ec82c8e629d2f
Jackie <actions@github.com>
2023-12-13 11:14:03 -0500

    Project 1 In Progress

--- src/components/Projects/Project01/Project01.jsx
+++ src/components/Projects/Project01/Project01.jsx
@@ -4,11 +4,31 @@ import background from '../../../asset/project/project01/background.png'
 
 const Project01 = () => {
   return (
-    <div className='project01'>
+    <div className='project01 project'>
       <div className="project01-overview">
-        <h4>User Centered Design</h4>
-        <h1>WinterBliss</h1>
-        <h2>Understanding and Addressing Mental Health Challenges In the Winter Of Toronto</h2>
+        <div className="project01-overview-top">
+          <h4>User Centered Design</h4>
+          <h1>WinterBliss</h1>
+          <h2>Understanding and Addressing Mental Health Challenges In the Winter Of Toronto</h2>
+          <div className="project01-overview-top-details">
+            <div className="project01-overview-top-details-overview">
+              <h2>Overview</h2>
+              <p>There is a simple fact that people’s life, working and study habits have drastically changed since the start of COVID-19 in 2020. While we do save a lot of time from commuting, but staying isolated at home, lacking physical interaction with peers, constant procrastination, and social media distractions throughout the day hinder our productivity and motivation.</p>
+            </div>
+            <div className="project01-overview-top-details-info">
+              <p><span>Time Frame: </span> Sep 2023 to Dec 2023 (12 Weeks)</p>
+              <p><span>Role: </span>Studio Lead during prototyping phase</p>
+              <p><span>Contribution: </span>Primary research, secondary research, persona, affinity diagram, ideation, mid-fidelity prototyping, presentation, etc</p>
+              <p><span>Key Competencies: </span>Figma, Canva, mobile interface design, qualitative research, quantitative research, etc</p>
+            </div>
+          </div>
+        </div>
+        <div className="project01-overview-bottom">
+    
+        </div>
+        <div className="test">
+          dewfijiefjeiofj
+        </div>
       </div>
     </div>
   )
