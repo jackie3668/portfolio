52cf02c48725aeb0031eed6a981be7435f073d7e
Jackie <actions@github.com>
2023-12-26 18:26:07 -0500

    Project 1 Edits

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         </div>
         <Routes>
           <Route path="/" element={<><Navbar /><Hero /><FeaturedGallery /></>} />
-          <Route path="/01" element={<Project01 />} />
+          <Route path="/portfolio/01" element={<Project01 />} />
         </Routes>
       </div>
     </Router>
